97e36e647629feebbc5e86e12e07b7b3ee1c1617
dmurr <diegocmurray@gmail.com>
2018-11-09 21:00:44 +0000

    changed class name to BaseModel, added save and to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 module
 """
 
-class BaseClass():
+class BaseModel():
     """ Parent of all other classes """
 
     def __init__(self, created_at=None, updated_at=None, id=None):
@@ -16,3 +16,17 @@ class BaseClass():
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        d = {}
+        for k,v in self.__dict__.items():
+            if v is not None:
+                d[k] = v
+        d['__class__'] = self.__class__.__name__
+
+        d['created_at'] = d['created_at'].isoformat()
+        d['updated_at'] = d['updated_at'].isoformat()
+        return d
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 import unittest
-from models.base_model import BaseClass
+from models.base_model import BaseModel
 """
 Unittesting for base_model
 """
 
-class TestBaseClass(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """
     instance id's should not be equal to one another
 
     """
     def test_id(self):
-        bm1 = BaseClass()
-        bm2 = BaseClass()
-        self.assertIsInstance(bm1, BaseClass)
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertIsInstance(bm1, BaseModel)
         self.assertTrue(hasattr(bm1, "id"))
         self.assertNotEqual(bm1.id, bm2.id)
         self.assertIsInstance(bm1.id, str)
-        self.assertIsInstance(bm2, BaseClass)
+        self.assertIsInstance(bm2, BaseModel)
         self.assertTrue(hasattr(bm2, "id"))
         self.assertIsInstance(bm2.id, str)
 
     def test_created_updated(self):
-        bm1 = BaseClass()
-        bm2 = BaseClass()
+        bm1 = BaseModel()
+        bm2 = BaseModel()
         self.assertEqual("created_at", "updated_at")
         print(self.created_at)
         print(self.updated_at)
