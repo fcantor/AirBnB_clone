4c38a4a69c7c990087cb75b89fb150b4b49d4bba
dmurr <diegocmurray@gmail.com>
2018-11-14 03:21:06 +0000

    Created classes that inherit BaseModel and added necessary imports

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 import json
 import models
 """
-engine
+Contains the FileStorage Class
 """
 
 class FileStorage:
---
+++ models/place.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Contains Place Class
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """ contains public attributes """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = i - 0
+    number_rooms = i - 0
+    number_bathrooms = i - 0
+    max_guest = i - 0
+    price_by_night = i - 0
+    latitude = i - 0.0
+    longitude = i - 0.00
+    amenity_ids = []
